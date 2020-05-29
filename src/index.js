d739e39ac9d3842247fa9379b572638bd59de46c
AleksandrSTNK <aleksandr.stnk@gmail.com>
2020-05-29 23:20:33 +0300

    Added structure core and components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import './scss/index.scss'
+import {Excel} from '@/components/excel/Excel';
+import './scss/index.scss';
 
-console.log('index.js')
+
+const excel = new Excel('#app', {
+  components: []
+});
+console.log('Excel', excel);

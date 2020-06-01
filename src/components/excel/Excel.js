2857f6f4bf216a08cb6655d1d405f46cd28b8227
AleksandrSTNK <aleksandr.stnk@gmail.com>
2020-06-01 15:39:18 +0300

    Added event listeners for components

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,6 +1,37 @@
+import {$} from '@core/dom';
+
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector);
+    this.$el = $(selector);
     this.components = options.components || [];
+  }
+
+  getRoot() {
+    // const $root = document.createElement('div');
+    // $root.classList.add('excel');
+    const $root = $.create('div', 'excel');
+    this.components = this.components.map( (Component) => {
+      // const $el = document.createElement('div');
+      // $el.classList.add(Component.className);
+      const $el = $.create('div', Component.className);
+      const component = new Component($el);
+      // $el.innerHTML = component.toHTML();
+      // DEBUG
+      if (component.name) {
+        window['c' + component.name] = component;
+      }
+      $el.html(component.toHTML());
+      $root.append($el);
+      // $root.insertAdjacentHTML('beforeend', component.toHTML());
+      return component;
+    });
+
+    return $root;
+  }
+
+  render() {
+    // this.$el.insertAdjacentHTML('afterbegin', `<h1>ТЕСТ</h1>`);
+    this.$el.append(this.getRoot());
+    this.components.forEach( (component) => component.init() );
   }
 }

dec5718c4145a2a5b370d9880e52deb85c5042ad
AleksandrSTNK <aleksandr.stnk@gmail.com>
2020-06-03 15:10:06 +0300

    Added resize column

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,11 +1,68 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
+import {$} from '@core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    });
+  }
+
   toHTML() {
     return createTable();
   }
+
+  onMousedown(event) {
+    // console.log(event.target.getAttribute('data-resize'));
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target);
+      // ну такое
+      // const $parent = $resizer.$el.parentNode;
+      // уже лучше
+      // const $parent = $resizer.$el.closest('[data-type="resizable"]');
+      const $parent = $resizer.closest('[data-type="resizable"]');
+      const coordinates = $parent.getCoordinates();
+
+      const cells = this.$root
+          .findAll(`[data-column="${$parent.data.column}"]`);
+
+      document.onmousemove = (e) => {
+        // Здесь всегда целочисленные значения,
+        // поэтому не нужен Math.floor()
+        const delta = e.pageX - coordinates.right;
+        // Считаем ширину колонки
+        const value = coordinates.width + delta;
+        $parent.$el.style.width = value + 'px';
+        // Это непроизводительно
+        // document.querySelectorAll(`[data-column="${$parent.data.column}"]`)
+        //     .forEach( (el) => {
+        //       el.style.width = value + 'px';
+        //     });
+        // Уже лучше
+        // this.$root.findAll(`[data-column="${$parent.data.column}"]`)
+        //     .forEach( (el) => {
+        //       el.style.width = value + 'px';
+        //     });
+        cells.forEach( (el) => {
+          el.style.width = value + 'px';
+        });
+      };
+
+      document.onmouseup = () => {
+        document.onmousemove = null;
+      };
+    }
+  }
 }
 
+
+// 316 msScripting
+// 5897 msRendering
+// 969 msPainting
+
+// 156 msScripting
+// 3700 msRendering
+// 559 msPainting
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,25 +3,34 @@ const CODES = {
   Z: 90
 };
 
-function toCell() {
+function toCell(_, column) {
   return `
-    <div class="cell" contenteditable></div>
-  `;
+    <div class="cell" contenteditable data-column="${column}"></div>
+  `.trim();
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">${col}</div>
-  `;
+    <div class="column" data-type="resizable" data-column="${index}">
+      ${col}
+      <div class="column-resize" data-resize="column"></div>
+    </div>
+  `.trim();
 }
 
 function createRow(index, content) {
+  const resize = index ?
+      `<div class="row-resize" data-resize="row"></div>` :
+      '';
   return `
     <div class="row">
-      <div class="row-info">${index ? index : ''}</div>
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
+      </div>
       <div class="row-data">${content}</div>
     </div>
-  `;
+  `.trim();
 }
 
 function toChar(_, index) {
